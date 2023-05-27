df6d4c045aea96dbbda3b0b337e84defbd0f164f
ShiharaKaumadi <eshani9544@gmail.com>
2023-05-27 09:35:15 +0530

    Add Quote

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,3 +64,10 @@ for(let i=0; i<imgBx.length; i++){
         this.className='imgBx active';
     })
 }
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+    cursor.setAttribute(
+        "style",
+        "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+    );
+});
