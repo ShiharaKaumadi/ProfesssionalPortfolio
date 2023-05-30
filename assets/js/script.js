e94428e434f64a45ac8e66c4fd3ec3399013930e
ShiharaKaumadi <eshani9544@gmail.com>
2023-05-30 16:45:09 +0530

    Completed Upto Projects Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 //Responsive NavBar
 const toggleIcon =document.querySelector(".nav-toggle");
 const menu =document.querySelector(".menu");
@@ -36,16 +38,7 @@ linkelement.forEach(function (linkItem){
 })
 
 /*========================================================*/
-/*
-const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    // reset: true
-})
-sr.reveal(`.specs_data`,{origin: 'left', interval: 100})
-sr.reveal(`.specs_img`,{origin: 'right'})
-*/
+
 /*===================================Skills========================*/
 //Skills Section
 let imgBx = document.querySelectorAll('.imgBx');
@@ -64,10 +57,44 @@ for(let i=0; i<imgBx.length; i++){
         this.className='imgBx active';
     })
 }
-const cursor = document.querySelector(".cursor");
+const cursor = document.querySelector(".cursor-skill");
 document.addEventListener("mousemove", (e) => {
     cursor.setAttribute(
         "style",
         "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
     );
 });
+
+/*============================================================================================================*/
+/* Projects Section*/
+
+$(document).ready(function(){
+
+    $('.buttonsP').click(function(){
+
+        $(this).addClass('act').siblings().removeClass('act');
+
+        var filter = $(this).attr('data-filter')
+
+        if(filter == 'all'){
+            $('.imageP').show(400);
+        }else{
+            $('.imageP').not('.'+filter).hide(200);
+            $('.imageP').filter('.'+filter).show(400);
+        }
+
+    });
+
+    $('.projects-section').magnificPopup({
+
+        delegate:'a',
+        type:'image',
+        gallery:{
+            enabled:true
+        }
+
+    });
+
+});
+
+/*=====================================================================================================================*/
